001a9fe0c976e995193dcd23cc88d1703f5ff626
Jinxiangyyu <yuannie06@gmail.com>
2023-06-05 09:41:24 -0700

    update

--- Workplace/FileSystem/src/FileSystem.java
+++ Workplace/FileSystem/src/FileSystem.java
@@ -8,25 +8,71 @@ public class FileSystem {
         root.setParent(root);
     }
 
-    public void mkDir(String path){}
-
-    public void createFile(String path){}
-
-    public void cd(String path){}
-
-    public void delete(String path){}
-
-    public void countFiles(){}
-
-    public void getSize(){}
+    public void mkDir(String path){
+      traverse(path, 0);
+    }
 
 
-    public void mkdir(String path) {
-        if (path == null || path.length() == 0) return;
+    public void createFile(String path, long size){//a/b/c.java
+        //不能走到最底，走到倒数第二个，对相同文件做处理
         String[] pathNames = path.split("/");
+        Node cur =  traverse(path, 1);
+        if (cur instanceof Directory ) {
+            File file = new File(pathNames[pathNames.length - 1], (Directory) cur, size, 777);
+            ((Directory) cur).getContent().add(file);
+        }else{
+            throw new RuntimeException("is a file");
+        }
+    }
+
+    public boolean cd(String path){
+        if (path == null || path.length() == 0) return false;
+        String[] pathNames = path.split("/"); //a/b/c
         Directory cur = root;
         boolean flag = false;
         for (String name : pathNames) {
+            // reset flag; ?
+            for (Node child : cur.getContent()) {
+                if (child instanceof Directory && child.name.equals(name)) {
+                    cur = (Directory) child;
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean delete(String path){//a/b/c/d
+        if (!cd(path)) {
+            return false;
+        }
+        Node deleteNode = traverse(path, 0);
+        Directory directory = (Directory) traverse(path, 1);
+        directory.deleteNode(deleteNode);
+        return true;
+    }
+
+    public int countFiles(){
+        return root.countFiles();
+    }
+
+    public long getSize(){
+        return root.getSize();
+    }
+
+
+
+    private Node traverse(String path, int lastLine) {
+        if (path == null || path.length() == 0) return root;
+        String[] pathNames = path.split("/");
+        Directory cur = root;
+        boolean flag = false;
+        for (int i = 0; i < pathNames.length - lastLine; i++) {
+            String name = pathNames[i];
             // reset flag; ?
             for (Node child : cur.getContent()) {
                 if (child instanceof Directory && child.name.equals(name)) {
@@ -41,6 +87,7 @@ public class FileSystem {
                 cur = newDir;
             }
         }
+        return cur;
     }
 }
 
--- Workplace/FileSystem/src/Main.java
+++ Workplace/FileSystem/src/Main.java
@@ -2,7 +2,18 @@ public class Main {
     public static void main(String[] args) {
 
         FileSystem fs = new FileSystem();
-        String path = "/Users/jyu/Downloads";
-        System.out.println("Hello world!");
+        String path1 = "/Users/jyu/Downloads";
+        String path2 = "/Users/jyu/Downloads/c.java";
+        fs.mkDir(path1);
+        fs.createFile(path2, 78965);
+        boolean res = fs.cd(path1);
+        int res1 = fs.countFiles();
+        long res2 = fs.getSize();
+        boolean res3 = fs.delete(path2);
+        System.out.println(res);
+        System.out.println(res1);
+        System.out.println(res2);
+        System.out.println(res3);
+
     }
 }
